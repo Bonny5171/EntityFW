9b73b0ef709df2bd7e61076140c240926d045e05
Luis Fernando <luis.city@gmail.com>
2018-07-10 12:16:32 -0300

    Criado class DAO disposable abrindo e fechando a conexao

---
+++ _EntityFW/ProdutoDAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _EntityFW
+{
+    class ProdutoDAO : IDisposable
+    {
+        private IDbConnection conexao;
+        public ProdutoDAO()
+        {
+            this.conexao = new System.Data.SqlClient
+                .SqlConnection("Data Source=DESKTOP-K6073DO;Initial Catalog=HondaFitParts;Persist Security Info=True;User ID=sa;Password=asdasd");
+            this.conexao.Open();
+        }
+        internal void Adicionar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+        internal void Atualizar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+        internal void Remover(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+        internal void Produtos()
+        {
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            this.conexao.Close();
+        }
+    }
+}
